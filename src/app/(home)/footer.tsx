9bc3e4a6f024b7cb1bf904f5183c32867917f3ba
Lê Tiến Đạt <letiendat1209@gmail.com>
2025-06-05 15:55:51 +0700

    03: home layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="flex border-t justify-between font-medium p-6">
+            <div className="flex items-center ">
+                <p>funroad, Inc.</p>
+            </div>
+        </footer>
+    );
+};
