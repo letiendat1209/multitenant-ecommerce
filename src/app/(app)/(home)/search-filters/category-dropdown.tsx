61ebc8c3465b2f881c457dd037c8c3e05acb2254
Lê Tiến Đạt <letiendat1209@gmail.com>
2025-06-06 13:35:31 +0700

    06: categories finalization

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,15 +1,16 @@
 "use client"
 
-import { Category } from "@/payload-types";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useRef, useState } from "react";
 
 import { useDropdownPosition } from "./use-dropdown-position";
 import { SubcategoryMenu } from "./subcategory-menu";
+import { CustomCategory } from "../types";
+import Link from "next/link";
 
 interface Props {
-    category: Category;
+    category: CustomCategory;
     isActive?: boolean;
     isNavigationHovered?: boolean;
 }
@@ -28,20 +29,33 @@ export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: Pr
     const onMouseLeave = () => setIsOpen(false);
     const dropdownPosition = getDropdownPosition();
 
+    // TODO: Protentially Improve Mobile
+    // const toggleDropdown = () => {
+    //     if (category.subcategories?.docs?.length) {
+    //         setIsOpen(!isOpen);
+    //     }
+    // }
+
     return (
         <div
             className="relative"
             ref={dropdownRef}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
+            // onClick={toggleDropdown}
             >
             <div className="Relative">
                 <Button
                     variant="elevated"
                     className={cn("h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-                    isActive && !isNavigationHovered && "bg-white border-primary")}
+                        isActive && !isNavigationHovered && "bg-white border-primary", isOpen && 
+                        "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]"
+                    )}
                 >
-                    {category.name}
+                    <Link
+                        href={`/${category.slug === "all" ? "" : category.slug}`}>
+                        {category.name}
+                    </Link>
                 </Button>
                 {category.subcategories && category.subcategories.length > 0 && (
                     <div
