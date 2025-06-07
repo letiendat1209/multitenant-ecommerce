e980dc56b002924e2c9a8d5bd1b06305197a799b
Lê Tiến Đạt <letiendat1209@gmail.com>
2025-06-07 23:28:34 +0700

    10: category pages

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,10 +1,11 @@
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
 import { getQueryClient, trpc } from "@/trpc/server";
-import { Footer } from "./footer";
-import { Navbar } from "./navbar";
-import { SearchFilters, SearchFiltersSkeleton } from "./search-filters";
 import { Suspense } from 'react';
+
+import { Footer } from "@/modules/home/ui/components/footer";
+import { SearchFilters, SearchFiltersSkeleton } from "@/modules/home/ui/components/search-filters";
+import { Navbar } from '@/modules/home/ui/components/navbar';
 
 interface Props {
     children: React.ReactNode;
--- src/modules/home/ui/components/search-filters/categories.tsx
+++ src/modules/home/ui/components/search-filters/categories.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useParams } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
 import { CategoryDropdown } from "./category-dropdown";
@@ -14,6 +15,9 @@ interface Props {
 };
 
 export const Categories = ({ data }: Props) => {
+    const params = useParams();
+
+
     const containerRef = useRef<HTMLDivElement>(null);
     const measureRef = useRef<HTMLDivElement>(null);
     const viewAllRef = useRef<HTMLDivElement>(null);
@@ -22,7 +26,8 @@ export const Categories = ({ data }: Props) => {
     const [isAnyHovered, setIsAnyHovered] = useState(false);
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const activeCategory = "all";
+    const categoryParam = params.category as string | undefined;
+    const activeCategory = categoryParam || "all";
 
     const activeCategoryIndex = data.findIndex((cat) => cat.slug === activeCategory);
     const isActiveCategoryHidden = activeCategoryIndex >= visibleCount && activeCategoryIndex !== -1;
@@ -94,6 +99,7 @@ export const Categories = ({ data }: Props) => {
                     ref={viewAllRef}
                     className="shirk-0 ">
                     <Button
+                        variant="elevated"
                         className={cn(
                             "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
                             isActiveCategoryHidden && !isAnyHovered && "bg-white border-primary",
