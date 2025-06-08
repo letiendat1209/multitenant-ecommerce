c7e09ceefc13ea1cf8f044abfbf52848ccf45b45
Lê Tiến Đạt <letiendat1209@gmail.com>
2025-06-08 22:45:58 +0700

    13: api filters and sorting

--- src/modules/products/hooks/use-product-filters.ts
+++ src/modules/products/hooks/use-product-filters.ts
@@ -1,14 +1,27 @@
-import { parseAsString, useQueryStates } from "nuqs"
+import { useQueryStates, parseAsArrayOf,parseAsString, parseAsStringLiteral } from "nuqs";
+
+const sortValues = ["curated", "trending", "hot_and_new"] as const;
+
+const params = {
+    sort: parseAsStringLiteral(sortValues).withDefault("curated"),
+    minPrice: parseAsString
+            .withOptions({
+                clearOnDefault: true,
+            })
+            .withDefault(""),
+    maxPrice: parseAsString
+            .withOptions({
+                clearOnDefault: true,
+            })
+            .withDefault(""),
+    tags: parseAsArrayOf(parseAsString)
+            .withOptions({
+                clearOnDefault: true
+            })
+            .withDefault([]),
+}
 
 export const useProductFilters = () => {
-    return useQueryStates({
-        minPrice: parseAsString
-            .withOptions({
-            clearOnDefault: true,
-            }),
-        maxPrice: parseAsString
-            .withOptions({
-            clearOnDefault: true,
-            }),
-    })
-}
+    return useQueryStates(params);
+};
+
