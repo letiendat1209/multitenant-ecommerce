3c077907e52f7865c503732be034a44da40daf68
Lê Tiến Đạt <letiendat1209@gmail.com>
2025-06-09 16:48:32 +0700

    14: product list ui

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -2,13 +2,16 @@ import { z } from "zod";
 import { Sort, Where } from "payload";
 
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { Category } from "@/payload-types";
+import { Category, Media } from "@/payload-types";
 import { sortValues } from "../search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
         .input(
             z.object({
+                cursor: z.number().default(1),
+                limit: z.number().default(DEFAULT_LIMIT),
                 category: z.string().nullable().optional(),
                 minPrice: z.string().nullable().optional(),
                 maxPrice: z.string().nullable().optional(),
@@ -16,83 +19,91 @@ export const productsRouter = createTRPCRouter({
                 sort: z.enum(sortValues).nullable().optional(),
             })
         ).query(async ({ ctx, input }) => {
-        const where: Where = {};
-        let sort: Sort = "-createdAt";
-        if (input.sort ==="curated") {
-            sort = "-createdAt";
-        }
-        if (input.sort ==="hot_and_new") {
-            sort = "+createdAt";
-        }
-        if (input.sort ==="trending") {
-            sort = "-createdAt";
-        }
-        if (input.minPrice && input.maxPrice) {
-            where.price = {
-            greater_than_equal: input.minPrice,
-            less_than_equal: input.maxPrice,
+            const where: Where = {};
+            let sort: Sort = "-createdAt";
+            if (input.sort === "curated") {
+                sort = "-createdAt";
             }
-        } else if (input.minPrice) {
-            where.price = {
-                greater_than_equal: input.minPrice,
+            if (input.sort === "hot_and_new") {
+                sort = "+createdAt";
             }
-        } else if (input.maxPrice) {
-            where.price = {
-                less_than_equal: input.maxPrice,
+            if (input.sort === "trending") {
+                sort = "-createdAt";
             }
-        }
+            if (input.minPrice && input.maxPrice) {
+                where.price = {
+                    greater_than_equal: input.minPrice,
+                    less_than_equal: input.maxPrice,
+                }
+            } else if (input.minPrice) {
+                where.price = {
+                    greater_than_equal: input.minPrice,
+                }
+            } else if (input.maxPrice) {
+                where.price = {
+                    less_than_equal: input.maxPrice,
+                }
+            }
         
-        if (input.category) {
-            const categoriesData = await ctx.db.find({
-                collection: 'categories',
-                limit: 1,
-                depth: 1, // Populate subcategories, subcategories.[0] will be a type of "Category"
-                pagination: false,
-                where: {
-                    slug: {
-                        equals: input.category,
-                    }
-                },
-            });
+            if (input.category) {
+                const categoriesData = await ctx.db.find({
+                    collection: 'categories',
+                    limit: 1,
+                    depth: 1, // Populate subcategories, subcategories.[0] will be a type of "Category"
+                    pagination: false,
+                    where: {
+                        slug: {
+                            equals: input.category,
+                        }
+                    },
+                });
 
-            const formattedData = categoriesData.docs.map((doc) => ({
-                ...doc,
-                subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-                    // Because of 'depth: 1' we are confident "doc" will be a type of "Category"
-                    ...(doc as Category),
-                    subcategories: undefined,
-                }))
-            }));
+                const formattedData = categoriesData.docs.map((doc) => ({
+                    ...doc,
+                    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+                        // Because of 'depth: 1' we are confident "doc" will be a type of "Category"
+                        ...(doc as Category),
+                        subcategories: undefined,
+                    }))
+                }));
 
-            const subcategoriesSlugs = [];
-            const parentCategory = formattedData[0];
+                const subcategoriesSlugs = [];
+                const parentCategory = formattedData[0];
 
-            if (parentCategory) {
-                subcategoriesSlugs.push(
-                    ...parentCategory.subcategories.map((subcategories) => subcategories.slug)
-                )
-                
                 if (parentCategory) {
-                where["category.slug"] = {
-                        in: [parentCategory.slug, ...subcategoriesSlugs],
+                    subcategoriesSlugs.push(
+                        ...parentCategory.subcategories.map((subcategories) => subcategories.slug)
+                    )
+                
+                    if (parentCategory) {
+                        where["category.slug"] = {
+                            in: [parentCategory.slug, ...subcategoriesSlugs],
+                        }
                     }
                 }
-            } 
-        }
-        
-        if (input.tags && input.tags.length > 0) {
-            where["tags.name"] = {
-                in: input.tags,
             }
-        }
+        
+            if (input.tags && input.tags.length > 0) {
+                where["tags.name"] = {
+                    in: input.tags,
+                }
+            }
             
-        const data = await ctx.db.find({
-            collection: 'products',
-            depth: 1, // Populate "category" & "images"
-            where,
-            sort,
-        });
+            const data = await ctx.db.find({
+                collection: 'products',
+                depth: 1, // Populate "category" & "images"
+                where,
+                sort,
+                page: input.cursor,
+                limit: input.limit,
+            });
 
-        return data;
-    })
-})
+            return {
+                ...data,
+                docs: data.docs.map((doc) => ({
+                    ...doc,
+                    image: doc.image as Media | null,
+                }))
+            }
+    }),
+});
